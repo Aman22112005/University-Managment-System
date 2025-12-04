6a7c6d3c8d66b15c46605dd2b09c3aa1cd0771e2
Aman Singh Bisht <pradeepgame3@gmail.com>
2025-12-04 20:51:51 +0530

    added

---
+++ Database.py
@@ -0,0 +1,34 @@
+import os
+from tkinter import *
+from dotenv import load_dotenv
+import mysql.connector
+
+dotenv_path = '/storage/emulated/0/Python/.env' 
+
+# Load the file using the full path
+load_dotenv() 
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT", "3306")
+
+def get_db_connection():
+	try:
+		conn = mysql.connector.connect(
+        	host=DB_HOST,
+        	port=int(DB_PORT),
+        	user=DB_USER,
+        	password=DB_PASSWORD,
+        	database=DB_NAME
+		)
+		return conn
+	except mysql.connector.Error as err:
+		print("DatabaseError: ", err)
+		return None
+	except Exception as err:
+		print("Error: ", err)
+		return None
+	
+	
---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from Database import get_db_connection
+
+app = Flask(__name__)
+
+conn = get_db_connection()
+
+@app.route("/")
+def hello():
+    return render_template("loginPage.html")
+
+@app.route('/loginsubmit', methods=['POST'])
+def submit():
+    try:
+        login_type = request.form.get('login_type')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        print(f"Login Type: {login_type}, Email: {email}, Password: {password}")
+        
+        # Ensure a response is always returned
+        return "Submitted"
+    except Exception as e:
+        print(f"Error in submit: {e}")
+        return "An error occurred", 500  # Return an error response
+
+@app.route('/register')
+def register():
+    try:
+        return render_template('Register.html')
+    except Exception as e:
+        print(f"Error: {e}")
+        return "An error occured", 500
+    
+@app.route('/registersubmit')
+def register():
+    try:
+        return render_template('Register.html')
+    except Exception as e:
+        print(f"Error: {e}")
+        return "An error occured", 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
